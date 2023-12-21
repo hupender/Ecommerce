12dbc1dbc3fa7a6cdf392ff47935a35147e7086c
hupender <hupenderkhatod>
2023-12-21 11:25:25 +0530

    add category_schema

---
+++ src/category_schema.js
@@ -0,0 +1,24 @@
+import mongoose, { Schema } from "mongoose";
+import pkg from 'validator';
+
+const {isEmail} = pkg;
+mongoose.connect("mongodb+srv://hupenderkhatod:Hupender%40123@cluster0.yl15wtm.mongodb.net/");
+
+// we can increase the id by 1 for each entry in the database
+const productsSchema = mongoose.Schema({
+    _id: Number,
+    name: String,
+    brand: String,
+    rating: Number,
+    category: String,
+    subcategory: String,
+    seller: [{
+        email: String,
+        price: String,
+        quantity: Number
+    }]
+});
+productsSchema.index({name:1},{brand:1});
+const products = mongoose.model("products",productsSchema);
+
+export default products;
